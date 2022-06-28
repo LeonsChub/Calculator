7b04b6e3ddd6824e67475c2cc8e59c603d88eb9a
Leon <pavlenkoleon@gmail.com>
2022-06-28 19:02:03 +0300

    js logic works alright

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ const multiply = function(num1,num2){
 };
 
 const operate = function(num1,num2,o){
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    
+
     if(o === '+'){
         return add(num1,num2)
     }
@@ -26,15 +30,9 @@ const operate = function(num1,num2,o){
     }
 
     else if(o === '/'){
-        return subtract(num1,num2)
+        return divide(num1,num2)
     }
 }
-
-
-console.log(add(2,2));
-console.log(subtract(3,2));
-console.log(divide(1,0));
-console.log(multiply(10,10));
 
 const btns = document.querySelectorAll("button");
 const result = document.querySelector(".output");
@@ -46,32 +44,62 @@ let operand = ""
 
 for(let btn of btns){
     btn.addEventListener("click",()=>{
+
         if(parseInt(btn.id)<10){
+
             output_val+=btn.id;
             result.textContent = output_val;
         }
-        else{
 
-            last_val = output_val;
-            output_val = "";
-            result.textContent = output_val;
+        else{
 
             switch(btn.id) {
                 case "plus":
                   operand = "+";
+                  last_val = output_val;
+                  output_val = "";
+                  result.textContent = output_val;
+
                   break;
+
                 case "minus":
+
                   operand = "-";
+                  last_val = output_val;
+                  output_val = "";
+                  result.textContent = output_val;
                   break;
+
                 case "divide":
+
                   operand = "/";
+                  last_val = output_val;
+                  output_val = "";
+                  result.textContent = output_val;
                   break;
+
                 case "times":
-                    operand = "x"
+                    
+                    operand = "x";
+                    last_val = output_val;
+                    output_val = "";
+                    result.textContent = output_val;
                     break;
+
                 case "=":
-                    console.log(operate(parseInt(last_val,output_val,operand)));
+                    ans = operate(last_val,output_val,operand);//answer
+                    result.textContent = ans;
+
+                    break;
+
+                case "clear":
+                    output_val = "";
+                    result.textContent = output_val;
+                    break;
               }
+            console.log("current value",output_val);
+            console.log("previous value",last_val);
+            
             }
     });
 }
